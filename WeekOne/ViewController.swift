4c9ef59f263170e2d9ee8600363eb81dcaa2576c
schmitck <schmitck@bc.edu>
2020-01-13 18:34:09 -0500

    First class work.

--- WeekOne/ViewController.swift
+++ WeekOne/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      print("We're running!")
+        messageLabel.text = "First Class 😁"
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        print("Button has been clicked")
+        messageLabel.text = "Moving on!"
+    }
+    
 }
 
